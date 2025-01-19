86957b9a663e585a9975f201f2709cacb23c0faa
ange-0608 <162269120@qq.com>
2025-01-19 17:02:58 +0800

    Add files via upload

---
+++ 功能测试用文件/超链接颜色.groovy
@@ -0,0 +1,18 @@
+<style>
+a:link {
+color: pink; /* 未访问的链接 */
+text-decoration: none;
+}
+a:visited {
+color: blue; /* 已访问的链接 */
+text-decoration: none;
+}
+a:hover {
+color: red; /* 鼠标悬停时的链接 */
+text-decoration: none;
+}
+a:active {
+color: black; /* 鼠标点击时的链接 */
+text-decoration: none;
+}
+</style>
